8f47839805fa7097305ade8266ba3897fe41f4cc
Dan Cross <dancross96@gmail.com>
2022-09-01 13:04:56 -0500

    added note selctor and contianer components and loaded them into app folder

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,12 @@
 
 <script>
 import NoteToolbar from './components/NoteToolbar'
+import NoteContainer from './components/NoteContainer'
 export default {
   components: {
-    NoteToolbar
+    NoteToolbar,
+    NoteContainer,
+    NoteContainer
   }
 }
 </script>
@@ -12,30 +15,7 @@ export default {
 <template>
   <div id="app">
     <NoteToolbar />
-    <div class="note-container">
-      <div class="note-selectors">
-        <div class="note-selector active">
-          <p class="note-selector-title">First note...</p>
-          <p class="note-selector-timestamp">Timestamp here...</p>
-        </div>
-        <div class="note-selector">
-          <p class="note-selector-title">Second note...</p>
-          <p class="note-selector-timestamp">Timestamp here...</p>
-        </div>
-        <div class="note-selector">
-          <p class="note-selector-title">Third note...</p>
-          <p class="note-selector-timestamp">Timestamp here...</p>
-        </div>
-      </div>
-      <div class="note-editor">
-        <p class="note-editor-info">Timestamp here...</p>
-        <textarea class="note-editor-input">
-          First note...
-
-          Note text here...
-        </textarea>
-      </div>
-    </div>
+    <NoteContainer />
   </div>
 </template>
 
